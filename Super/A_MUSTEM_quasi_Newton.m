e9812b9b109c88964de2b5b84c634476b972d2fe
HengLiu2021 <hengliu.henry@gmail.com>
2021-06-21 19:52:29 -0400

    Update A_MUSTEM_quasi_Newton.m

--- Super/A_MUSTEM_quasi_Newton.m
+++ Super/A_MUSTEM_quasi_Newton.m
@@ -5,20 +5,13 @@ fold = 1;
 load(['./Samples\Samples_Labels_fold_',num2str(fold),'.mat']);
 
 X = squeeze(Samples);
-
 X_norm = (X(1:23,:)-min(X(1:23,:),[],2))./(max(X(1:23,:),[],2)-min(X(1:23,:),[],2));
 % expand a unit row to indicate constant term in Beta
 X_norm = [ones(1,length(Labels));X_norm];
 %
 
-%
-m = 24;
-n = 6;
-%
-
 I = X(end,:)';
 J = Labels;
-
 
 Beta_0 = importdata('../Deck\Beta_quasiNewton_fold_1.mat');
 options = optimoptions('fminunc','Display','iter','Algorithm','quasi-newton','SpecifyObjectiveGradient',true);
@@ -26,11 +19,11 @@ options = optimoptions('fminunc','Display','iter','Algorithm','quasi-newton','Sp
 my_fun = @(Beta)(MUSTEM_Loss_wGrad(Beta, X_norm, I, J));
 
 Beta = fminunc(my_fun,Beta_0,options);
-save(['Beta_quasiNewton_fold_',num2str(fold)],'Beta');
+%save(['Beta_quasiNewton_fold_',num2str(fold)],'Beta');
 
 X_train_max = max(X(1:23,:),[],2);
 X_train_min = min(X(1:23,:),[],2);
-save(['X_train_maxmin_fold_',num2str(fold),'X_train_max','X_train_min']);
+%save(['X_train_maxmin_fold_',num2str(fold)],'X_train_max','X_train_min');
 
 
 for fold = 2:10
@@ -38,15 +31,9 @@ for fold = 2:10
 
 
     X = squeeze(Samples);
-
     X_norm = (X(1:23,:)-min(X(1:23,:),[],2))./(max(X(1:23,:),[],2)-min(X(1:23,:),[],2));
     % expand a unit row to indicate constant term in Beta
     X_norm = [ones(1,length(Labels));X_norm];
-    %
-
-    %
-    m = 24;
-    n = 6;
     %
 
     I = X(end,:)';
@@ -58,11 +45,11 @@ for fold = 2:10
     my_fun = @(Beta)(MUSTEM_Loss_wGrad(Beta, X_norm, I, J));
 
     Beta = fminunc(my_fun,Beta_0,options);
-    save(['Beta_quasiNewton_fold_',num2str(fold)],'Beta');
+    %save(['Beta_quasiNewton_fold_',num2str(fold)],'Beta');
     
     X_train_max = max(X(1:23,:),[],2);
     X_train_min = min(X(1:23,:),[],2);
-    save(['X_train_maxmin_fold_',num2str(fold)],'X_train_max','X_train_min');
+    %save(['X_train_maxmin_fold_',num2str(fold)],'X_train_max','X_train_min');
 end
 
 

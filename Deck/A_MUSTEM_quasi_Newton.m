838276b072e2bebce94847b97d11ccfc3f09f0c4
HengLiu2021 <hengliu.henry@gmail.com>
2021-06-21 19:52:21 -0400

    Update A_MUSTEM_quasi_Newton.m

--- Deck/A_MUSTEM_quasi_Newton.m
+++ Deck/A_MUSTEM_quasi_Newton.m
@@ -4,17 +4,10 @@ clear
 fold = 1;
 load(['./Samples\Samples_Labels_fold_',num2str(fold),'.mat']);
 
-
 X = squeeze(Samples);
-
 X_norm = (X(1:23,:)-min(X(1:23,:),[],2))./(max(X(1:23,:),[],2)-min(X(1:23,:),[],2));
 % expand a unit row to indicate constant term in Beta
 X_norm = [ones(1,length(Labels));X_norm];
-%
-
-%
-m = 24;
-n = 6;
 %
 
 I = X(end,:)';
@@ -27,27 +20,20 @@ options = optimoptions('fminunc','Display','iter','Algorithm','quasi-newton','Sp
 my_fun = @(Beta)(MUSTEM_Loss_wGrad(Beta, X_norm, I, J));
 
 Beta = fminunc(my_fun,Beta_0,options);
-save(['Beta_quasiNewton_fold_',num2str(fold)],'Beta');
+%save(['Beta_quasiNewton_fold_',num2str(fold)],'Beta');
 
 X_train_max = max(X(1:23,:),[],2);
 X_train_min = min(X(1:23,:),[],2);
-save(['X_train_maxmin_fold_',num2str(fold)]);
+%save(['X_train_maxmin_fold_',num2str(fold)],'X_train_max','X_train_min');
 
 
 for fold = 2:10
     load(['./Samples\Samples_Labels_fold_',num2str(fold),'.mat']);
-
-
+    
     X = squeeze(Samples);
-
     X_norm = (X(1:23,:)-min(X(1:23,:),[],2))./(max(X(1:23,:),[],2)-min(X(1:23,:),[],2));
     % expand a unit row to indicate constant term in Beta
     X_norm = [ones(1,length(Labels));X_norm];
-    %
-
-    %
-    m = 24;
-    n = 6;
     %
 
     I = X(end,:)';
@@ -60,11 +46,11 @@ for fold = 2:10
     my_fun = @(Beta)(MUSTEM_Loss_wGrad(Beta, X_norm, I, J));
 
     Beta = fminunc(my_fun,Beta_0,options);
-    save(['Beta_quasiNewton_fold_',num2str(fold)],'Beta');
+    %save(['Beta_quasiNewton_fold_',num2str(fold)],'Beta');
     
     X_train_max = max(X(1:23,:),[],2);
     X_train_min = min(X(1:23,:),[],2);
-    save(['X_train_maxmin_fold_',num2str(fold)],'X_train_max','X_train_min');
+    %save(['X_train_maxmin_fold_',num2str(fold)],'X_train_max','X_train_min');
 end
 
 
